ba872f5d4a225527438a6117e4e963bf11f1dd88
Mihir Shete <mihirsht@gmail.com>
2015-11-23 16:42:16 -0600

    640: add argument parsing support
    Add argument parsing for blaster and blastee.
    TODO - verify if the arguments are legal.

--- blastee.c
+++ blastee.c
@@ -1,8 +1,37 @@
-
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+
+void blastee_usage(void) {
+    printf("usage: blastee -p <port> -c <echo 0|1>\n");
+    exit(1);
+}
 
 int main(int argc, char *argv[])
 {
-    printf("stub for blastee\n");
+    unsigned long int port = UINT_MAX, echo = UINT_MAX;
+    int ch;
+
+    if (argc < 5) {
+        blastee_usage();
+    }
+    while ((ch = getopt(argc, argv, "p:c:")) != -1) {
+        switch (ch) {
+            case 'p':
+                port = strtoul(optarg, NULL, 10);
+                break;
+            case 'c':
+                echo = strtoul(optarg, NULL, 10);
+                if (echo != 0 && echo != 1) {
+                    blastee_usage();
+                }
+                break;
+            case '?':
+            default:
+                blastee_usage();
+            }
+    }
+    printf("Echo: %s, port: %lu\n", echo == 1 ? "yes": "no", port);
     return 0;
 }
--- blaster.c
+++ blaster.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+void blaster_usage(void) {
+    printf("usage: blaster -s <hostname> -p <port> -r <rate> -n <num> -q <seq_no> -l <length> -c <echo>\n");
+    exit(1);
+}
 
 int main(int argc, char *argv[])
 {
-    printf("stub for blaster\n");
+    unsigned long port = UINT_MAX, echo = UINT_MAX;
+    unsigned long rate, numpkts, startseq, pktlen;
+    char *hostname;
+    int ch;
+
+    if (argc < 15) {
+        blaster_usage();
+    }
+
+    while ((ch = getopt(argc, argv, "s:p:r:n:q:l:c:")) != -1) {
+        switch (ch) {
+            case 's':
+                hostname = strdup(optarg);
+                break;
+            case 'p':
+                port = strtoul(optarg, NULL, 10);
+                break;
+            case 'r':
+                rate = strtoul(optarg, NULL, 10);
+                break;
+            case 'n':
+                numpkts = strtoul(optarg, NULL, 10);
+                break;
+            case 'q':
+                startseq = strtoul(optarg, NULL, 10);
+                break;
+            case 'l':
+                pktlen = strtoul(optarg, NULL, 10);
+                break;
+            case 'c':
+                echo = strtoul(optarg, NULL, 10);
+                if (echo != 0 && echo != 1) {
+                    blaster_usage();
+                }
+                break;
+            case '?':
+            default:
+                blaster_usage();
+            }
+    }
+
+    printf("hostname: %s, port: %lu, rate: %lu, numpkts: %lu, "
+            "startseq: %lu, pktlen: %lu, echo: %s\n", hostname, port, rate,
+            numpkts, startseq, pktlen, echo == 1 ? "yes": "no");
+
+    free(hostname);
+
     return 0;
 }
