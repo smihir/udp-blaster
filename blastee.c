aeb680bae925262bb6d842c2de267341ce4f6d47
Mihir Shete <mihirsht@gmail.com>
2015-11-30 19:09:09 -0600

    640: add echo functionality and timeout
    Add echo functionality to blaster and blastee.
    Blaster needs a thread to receive echo packets
    as recvfrom() is a blocking call and we do
    not want to affect tx if rx is stalling in the
    blaster. As per the spec the blastee should timeout
    if no packets are received for 5s, add this functionality
    as well.

--- blastee.c
+++ blastee.c
@@ -11,54 +11,17 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <sys/errno.h>
 
 #define BUFFER_SIZE 1500
-
-void run_blastee(char*  port);
-
 
 void blastee_usage(void) {
     printf("usage: blastee -p <port> -c <echo 0|1>\n");
     exit(1);
 }
 
-
-
-int main(int argc, char *argv[])
-{
-    unsigned long int port = UINT_MAX, echo = UINT_MAX;
-    char *port_str;
-    int ch;
-
-    if (argc < 5) {
-        blastee_usage();
-    }
-    while ((ch = getopt(argc, argv, "p:c:")) != -1) {
-        switch (ch) {
-            case 'p':
-                port = strtoul(optarg, NULL, 10);
-                port_str=strdup(optarg);
-                break;
-            case 'c':
-                echo = strtoul(optarg, NULL, 10);
-                if (echo != 0 && echo != 1) {
-                    blastee_usage();
-                }
-                break;
-            case '?':
-            default:
-                blastee_usage();
-            }
-    }
-
-    run_blastee(port_str);
-
-    printf("Echo: %s, port: %lu\n", echo == 1 ? "yes": "no", port);
-    return 0;
-}
-
-
-void run_blastee(char* port){
+void run_blastee(char* port, unsigned long int echo){
 
 	struct addrinfo hints, *res, *p;
 	int status;
@@ -66,6 +29,7 @@ void run_blastee(char* port){
 	int socketfd;
 	char *buffer=(char *)malloc(sizeof(char)*BUFFER_SIZE);
 	int flag=0;
+    struct timeval tv;
 
 	struct sockaddr src_addr;
 	socklen_t addrlen;
@@ -107,16 +71,83 @@ void run_blastee(char* port){
 		perror("Socket open failed: ");
 	}
 
+    //SPEC: exit if no packet for 5 secs
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+    if (setsockopt(socketfd, SOL_SOCKET, SO_RCVTIMEO,&tv,sizeof(tv)) < 0) {
+            perror("Error");
+    }
+
 	if(bind(socketfd, p->ai_addr, p->ai_addrlen)!=0){
 		perror("Socket binding failed");
 		return;
 	}
 
 	while(1){
-		recvfrom(socketfd, buffer, BUFFER_SIZE, flag, &src_addr, &addrlen);
+        int pktlen;
+        struct sockaddr_in *sin;
+		char *ipver;
+		void *addr;
+
+		pktlen = recvfrom(socketfd, buffer, BUFFER_SIZE, flag, &src_addr, &addrlen);
 		printf("Recived packet from: %s\n",buffer);
+        if (echo == 1 && pktlen != -1) {
+            sin = (struct sockaddr_in *)&src_addr;
+            if (p->ai_family == AF_INET) { // IPv4
+			    struct sockaddr_in *ipv4 = (struct sockaddr_in *) p->ai_addr;
+			    addr = &(ipv4->sin_addr);
+                ipver = "IPv4";
+            } else { // IPv6
+			    struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) p->ai_addr;
+			    addr = &(ipv6->sin6_addr);
+                ipver = "IPv6";
+            }
+		    inet_ntop(sin->sin_family, addr, ipstr, sizeof ipstr);
+		    printf("  %s: %s\n", ipver, ipstr);
+            addrlen = sizeof(src_addr);
+		    if (sendto(socketfd, buffer, pktlen, flag, &src_addr, addrlen) == -1) {
+		        perror("Send Error:");
+            }
+        }
+
+        //SPEC: exit if no packet for 5 secs
+        if (pktlen == -1 && errno == EAGAIN)
+            break;
 	}
 
 	freeaddrinfo(res);
+}
 
+int main(int argc, char *argv[])
+{
+    unsigned long int port = UINT_MAX, echo = UINT_MAX;
+    char *port_str;
+    int ch;
+
+    if (argc < 5) {
+        blastee_usage();
+    }
+    while ((ch = getopt(argc, argv, "p:c:")) != -1) {
+        switch (ch) {
+            case 'p':
+                port = strtoul(optarg, NULL, 10);
+                port_str = strdup(optarg);
+                break;
+            case 'c':
+                echo = strtoul(optarg, NULL, 10);
+                if (echo != 0 && echo != 1) {
+                    blastee_usage();
+                }
+                break;
+            case '?':
+            default:
+                blastee_usage();
+            }
+    }
+
+    printf("Echo: %s, port: %lu\n", echo == 1 ? "yes": "no", port);
+
+    run_blastee(port_str, echo);
+
+    return 0;
 }
--- blaster.c
+++ blaster.c
@@ -8,16 +8,115 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include "packet.h"
 
 #define BUFFER_SIZE 1500
-
-void run_blaster(char* hostname, char* port, int numpkts, int pktlen, int rate);
-
+int rx = 1;
 
 void blaster_usage(void) {
     printf("usage: blaster -s <hostname> -p <port> -r <rate> -n <num> -q <seq_no> -l <length> -c <echo>\n");
     exit(1);
+}
+
+void *blaster_echo_rx(void *arg)
+{
+    int sockfd = *(int *)arg;
+	char *buffer=(char *)malloc(sizeof(char)*BUFFER_SIZE);
+    int flag = 0;
+	struct sockaddr src_addr;
+	socklen_t addrlen;
+    struct timeval tv;
+
+    tv.tv_sec = 10;
+    tv.tv_usec = 0;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv,sizeof(tv)) < 0) {
+            perror("Error");
+    }
+
+	while(rx){
+        int pktlen = 0;
+		pktlen = recvfrom(sockfd, buffer, BUFFER_SIZE, flag, &src_addr, &addrlen);
+        if (pktlen == 0)
+		    printf("rx packet timeout\n");
+        else if (pktlen == -1)
+            perror("rx packet error: ");
+        else
+            printf("rx packet from: \n");
+	}
+
+    return NULL;
+}
+
+void run_blaster(char* hostname, char *port, int numpkts, int pktlen,
+        int rate, int echo)
+{
+	struct addrinfo hints, *res, *p;
+	int status;
+	char ipstr[INET_ADDRSTRLEN];
+	int socketfd;
+	char *buffer=(char *)malloc(sizeof(char)*pktlen);
+	int flag=0;
+    pthread_t rx_thread;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET; // AF_INET or AF_INET6 to force version
+	hints.ai_socktype = SOCK_DGRAM;
+
+	if ((status = getaddrinfo(hostname, port, &hints, &res)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+		return;
+	}
+
+	for (p = res; p != NULL; p = p->ai_next) {
+		void *addr;
+		char *ipver;
+
+		// get the pointer to the address itself,
+		// different fields in IPv4 and IPv6:
+		if (p->ai_family == AF_INET) { // IPv4
+			struct sockaddr_in *ipv4 = (struct sockaddr_in *) p->ai_addr;
+			addr = &(ipv4->sin_addr);
+			ipver = "IPv4";
+		} else { // IPv6
+			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) p->ai_addr;
+			addr = &(ipv6->sin6_addr);
+			ipver = "IPv6";
+		}
+
+		// convert the IP to a string and print it:
+		inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+		printf("  %s: %s\n", ipver, ipstr);
+		break;
+	}
+
+	socketfd=socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+	if(socketfd < 0 ){
+		perror("Socket open failed: ");
+	}
+
+	useconds_t time=1000000 / rate;
+	memcpy(buffer,"test",5);
+
+    if (echo == 1) {
+        pthread_create(&rx_thread, NULL, blaster_echo_rx, &socketfd);
+    }
+
+	while(numpkts--){
+		int ret;
+		ret=sendto(socketfd, buffer, pktlen, flag, p->ai_addr, p->ai_addrlen);
+		if(ret == -1){
+			perror("Send error: ");
+		}
+		printf("Send packet to: %s\n",ipstr);
+		usleep(time);
+	}
+
+    rx = 0;
+
+    if (echo == 1) {
+        pthread_join(rx_thread, NULL);
+    }
 }
 
 int main(int argc, char *argv[])
@@ -69,78 +168,9 @@ int main(int argc, char *argv[])
             "startseq: %lu, pktlen: %lu, echo: %s\n", hostname, port, rate,
             numpkts, startseq, pktlen, echo == 1 ? "yes": "no");
 
-    run_blaster(hostname, port_str, numpkts, pktlen, rate);
-
+    run_blaster(hostname, port_str, numpkts, pktlen, rate, echo);
 
     free(hostname);
 
     return 0;
-}
-
-
-void run_blaster(char* hostname, char *port, int numpkts, int pktlen, int rate){
-
-	struct addrinfo hints, *res, *p;
-	int status;
-	char ipstr[INET_ADDRSTRLEN];
-	int socketfd;
-	char *buffer=(char *)malloc(sizeof(char)*pktlen);
-	int flag=0;
-
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_INET; // AF_INET or AF_INET6 to force version
-	hints.ai_socktype = SOCK_DGRAM;
-
-	if ((status = getaddrinfo(hostname, port, &hints, &res)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-		return;
-	}
-
-	for (p = res; p != NULL; p = p->ai_next) {
-		void *addr;
-		char *ipver;
-
-		// get the pointer to the address itself,
-		// different fields in IPv4 and IPv6:
-		if (p->ai_family == AF_INET) { // IPv4
-			struct sockaddr_in *ipv4 = (struct sockaddr_in *) p->ai_addr;
-			addr = &(ipv4->sin_addr);
-			ipver = "IPv4";
-		} else { // IPv6
-			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) p->ai_addr;
-			addr = &(ipv6->sin6_addr);
-			ipver = "IPv6";
-		}
-
-		// convert the IP to a string and print it:
-		inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
-		printf("  %s: %s\n", ipver, ipstr);
-		break;
-	}
-
-	socketfd=socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-	if(socketfd < 0 ){
-		perror("Socket open failed: ");
-	}
-	//	if(bind(socketfd, p->ai_addr, p->ai_addrlen)!=0){
-//		perror("Socket binding failed");
-//		return;
-//	}
-	useconds_t time=1000000 / rate;
-//	printf()
-	memcpy(buffer,"test",5);
-	while(numpkts--){
-		int ret;
-		ret=sendto(socketfd, buffer, pktlen, flag, p->ai_addr, p->ai_addrlen);
-		if(ret == -1){
-			perror("Send error: ");
-		}
-		printf("Send packet to: %s\n",ipstr);
-		usleep(time);
-		//time+=1000.5;
-	}
-
-
-
-
 }
